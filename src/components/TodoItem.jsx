ce11fb14b3680be9d89c2d37f8ec441afc72ffa8
Tyler Martinez <slightlytyler@gmail.com>
2015-09-22 15:28:58 -0500

    bind checkbox to isComplete

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -13,6 +13,7 @@ export default React.createClass({
     return <li className={classes}>
       <input type="checkbox"
              className="toggle"
+             checked={this.props.todo.get('isComplete')}
              onClick={() => this.props.toggle(this.props.todo)}/>
       <label>{this.props.todo.get('title')}</label>
       <button className="destroy"></button>
