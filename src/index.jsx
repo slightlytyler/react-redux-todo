b5b07b06dba3a9772ea373a4c90a22438345b7a3
Tyler Martinez <slightlytyler@gmail.com>
2015-09-22 15:20:44 -0500

    dummy immutable data

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { fromJS } from 'immutable';
+
 import Main from './components/Main';
 
 
 require('./styles.css');
 
-const todos = [
+const todos = fromJS([
   {
     title: 'Learn react and redux',
     isComplete: true
@@ -17,7 +19,7 @@ const todos = [
     title: 'Profit',
     isComplete: false
   },
-];
+]);
 
 React.render(
   <div>
